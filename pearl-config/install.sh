8e5cce0b6e63812183dc67dc381ebf491f8820d3
Filippo Squillace <squillace.filippo@gmail.com>
2019-09-07 11:41:09 +1000

    first commit

---
+++ pearl-config/install.sh
@@ -0,0 +1,27 @@
+function post_install(){
+    pearl emerge ${PEARL_PKGREPONAME}/dot-tmux
+    pearl emerge ${PEARL_PKGREPONAME}/tpm
+    pearl emerge ${PEARL_PKGREPONAME}/tpm-extra
+    pearl emerge ${PEARL_PKGREPONAME}/tmux-env
+    pearl emerge ${PEARL_PKGREPONAME}/tmux-multi
+
+    return 0
+}
+
+function post_update(){
+    post_install
+
+    return 0
+}
+
+function pre_remove(){
+    if ask "Do you want to remove all tmux related packages?" "N"
+    then
+        pearl remove ${PEARL_PKGREPONAME}/dot-tmux
+        pearl remove ${PEARL_PKGREPONAME}/tpm
+        pearl remove ${PEARL_PKGREPONAME}/tpm-extra
+        pearl remove ${PEARL_PKGREPONAME}/tmux-env
+        pearl remove ${PEARL_PKGREPONAME}/tmux-multi
+    fi
+    return 0
+}
