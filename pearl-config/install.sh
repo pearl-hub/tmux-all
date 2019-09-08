256fe51a689aa249feca625f9d6434e92593d604
Filippo Squillace <squillace.filippo@gmail.com>
2019-09-08 15:40:10 +1000

    Handle dependencies with `DEPENDS` variable

--- pearl-config/install.sh
+++ pearl-config/install.sh
@@ -1,10 +1,6 @@
+DEPENDS=(${PEARL_PKGREPONAME}/dot-tmux ${PEARL_PKGREPONAME}/tpm ${PEARL_PKGREPONAME}/tpm-extra ${PEARL_PKGREPONAME}/tmux-env ${PEARL_PKGREPONAME}/tmux-multi ${PEARL_PKGREPONAME}/txum)
+
 function post_install(){
-    pearl emerge ${PEARL_PKGREPONAME}/dot-tmux
-    pearl emerge ${PEARL_PKGREPONAME}/tpm
-    pearl emerge ${PEARL_PKGREPONAME}/tpm-extra
-    pearl emerge ${PEARL_PKGREPONAME}/tmux-env
-    pearl emerge ${PEARL_PKGREPONAME}/tmux-multi
-    pearl emerge ${PEARL_PKGREPONAME}/txum
 
     return 0
 }
@@ -18,12 +14,10 @@ function post_update(){
 function pre_remove(){
     if ask "Do you want to remove all tmux related packages?" "N"
     then
-        pearl remove ${PEARL_PKGREPONAME}/dot-tmux
-        pearl remove ${PEARL_PKGREPONAME}/tpm
-        pearl remove ${PEARL_PKGREPONAME}/tpm-extra
-        pearl remove ${PEARL_PKGREPONAME}/tmux-env
-        pearl remove ${PEARL_PKGREPONAME}/tmux-multi
-        pearl remove ${PEARL_PKGREPONAME}/txum
+        for dep in ${DEPENDS[@]}
+        do
+            pearl remove $dep
+        done
     fi
     return 0
 }
